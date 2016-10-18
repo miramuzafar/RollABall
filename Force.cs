684e784a4429f13581e2837c8ece64640506b5a6
Mira <nurizzatulamiramuzafar@gmail.com>
2016-10-18 11:56:50 +0800

    added AddForce()

--- Force.cs
+++ Force.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class Force : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public float moveSpeed = 5;
+	bool isAlreadyClicked = false;
+
+	/*void Start() 
+    {
+        //rb = GetComponent<Rigidbody>();
+		//GetComponent<Rigidbody2D>().AddForce (Vector2, ForceMode2D.Impulse);
+		//GetComponent<Rigidbody2D>().AddForce(new Vector2(moveSpeed,0));
+    }*/
+
+	void Update()
+	{
+		if(Input.GetMouseButton(0))
+		{
+			if(!isAlreadyClicked)
+			{
+				//gameObject.GetComponent<Rigidbody2D>().AddForce (Vector2, ForceMode2D.Impulse);
+				GetComponent<Rigidbody2D>().AddForce(new Vector2(moveSpeed,0));
+			}
+			isAlreadyClicked = true;	
+		}
 	}
 }
