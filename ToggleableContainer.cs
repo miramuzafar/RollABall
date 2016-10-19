fbdbe6fc7b30e507021a458afa2aa8b49414af95
Mira <nurizzatulamiramuzafar@gmail.com>
2016-10-19 09:05:10 +0800

    created new script

---
+++ ToggleableContainer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleableContainer : MonoBehaviour {
+
+	 ToggleableItem _currentSelection;
+
+    // On initialization, find all toggleable children of this parent.
+    void Start() {
+       ToggleableItem[] children = GetComponentsInChildren<ToggleableItem>();
+       // Let each child know who to talk to when they're clicked:
+       foreach(var child in children)
+           child.SetContainer(this);
+    }
+
+    // When one platform is clicked, disable it and re-enable the last selection (if any).
+    public void Select(ToggleableItem target) {
+       if(target == _currentSelection)
+          return;
+
+        if(_currentSelection != null)
+            _currentSelection.gameObject.SetActive(true);
+
+        if(target != null)
+            target.gameObject.SetActive(false);
+
+        _currentSelection = target;
+    }    
+}
