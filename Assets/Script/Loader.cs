ce4eca41ef3ca2f08f496428b100d6077ff34c09
Mira <nurizzatulamiramuzafar@gmail.com>
2016-10-28 10:41:37 +0800

    load the levels available in the prefabs

---
+++ Assets/Script/Loader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class Loader : MonoBehaviour {
+
+	public static Loader instance; //Instance
+	public int currentLevel;
+	public GameObject[] levels;
+	
+
+	// Use this for initialization
+	void Awake () {
+
+		instance = this;
+		LoadLevel();
+	}
+
+	void LoadLevel()
+	{
+		if (currentLevel < levels.Length) {
+			Instantiate (levels [currentLevel]);
+		}
+	}
+}
