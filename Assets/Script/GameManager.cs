6ae608543422fb5c0aa50b5302464cf633dac42b
Mira <nurizzatulamiramuzafar@gmail.com>
2016-10-28 10:38:20 +0800

    created game manager, handles the main menu ui

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	
+	public void StartLevel()
+	{
+		Debug.Log("clicked");
+		SceneManager.LoadScene(1);
+	}
+	public void ExitGame()
+	{
+		Debug.Log("exit");
+		Application.Quit ();
+	}
+}
