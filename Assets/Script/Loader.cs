9a17f2219b80cf230b99d1524e7e9f1b2d6fb187
Mira <nurizzatulamiramuzafar@gmail.com>
2016-11-09 16:14:38 +0800

    updated the list for the level prefab

--- Assets/Script/Loader.cs
+++ Assets/Script/Loader.cs
@@ -10,14 +10,14 @@ public class Loader : MonoBehaviour {
 	public static int currentLevel;
 	//public GameObject[] levels;
 	public static Loader instance;
-	public List<GameObject> myList;
+	public List<GameObject> myList = new List<GameObject>();
 	
 
 	// Use this for initialization
 	void Awake () {
 		LoadLevel();
 		instance = this;
-	//	myList = levels.ToList();
+		//myList = levels.ToList();
 		
 	}
 
@@ -28,21 +28,22 @@ public class Loader : MonoBehaviour {
 			levels[p] = Resources.Load("Prefabs" + p) as GameObject;
 		}*/
 		//Instantiate(levels[currentLevel]);
-		if (PlayerPrefs.HasKey ("CurrentLevel") == false) {
-			PlayerPrefs.SetInt("CurrentLevel",1);
-			currentLevel=PlayerPrefs.GetInt ("CurrentLevel");
+		if (PlayerPrefs.HasKey ("currentLevel") == false) {
+			PlayerPrefs.SetInt("currentLevel",1);
+			currentLevel=PlayerPrefs.GetInt ("currentLevel");
 		}
-		currentLevel = PlayerPrefs.GetInt ("CurrentLevel");
+		currentLevel = PlayerPrefs.GetInt ("currentLevel");
 		//if (currentLevel < myList.Length) {
 			Instantiate (myList [currentLevel]);
+		//myList.Add(GameObject.Find(myList));
 		//}
 	}
 	public void NextLevel()
 	{
 		currentLevel++;
-		PlayerPrefs.SetInt("CurrentLevel",currentLevel);
+		PlayerPrefs.SetInt("currentLevel",currentLevel);
 		//if (currentLevel < myList.Length) {
-			PlayerPrefs.SetInt("Level" +currentLevel,1);
+			PlayerPrefs.SetInt("Level " +currentLevel,1);
 		//}
 		//else
 		//{
