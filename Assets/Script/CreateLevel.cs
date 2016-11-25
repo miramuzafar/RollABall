22af35e80d25df5b8ac4546a77727fd6a6afc5c2
Mira <nurizzatulamiramuzafar@gmail.com>
2016-11-25 18:42:24 +0800

    created new script for level prefab

---
+++ Assets/Script/CreateLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CreateLevel : MonoBehaviour {
+
+	public GameObject[] Levels;
+	public static int currentLevel = 0;
+
+	// Use this for initialization
+	void Start () {
+		GameObject obj = Instantiate(Levels[currentLevel-1]) as GameObject;
+		//PlayerPrefs.SetInt ("Level ",1);
+		//currentLevel=PlayerPrefs.GetInt ("Level ");
+		//obj.transform.SetParent(transform,false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
