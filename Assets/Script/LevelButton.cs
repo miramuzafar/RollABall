45ee40be8832a2bb545694b6f7e159fc5befd00c
Mira <nurizzatulamiramuzafar@gmail.com>
2016-12-29 21:11:55 +0800

    created button class

---
+++ Assets/Script/LevelButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButton : MonoBehaviour {
+
+	public Text levelText;
+	public int unlocked;
+	/*public GameObject star1;
+	public GameObject star2;
+	public GameObject star3;*/
+}
