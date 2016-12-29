13d59b19ee0f5acbfa35cbfac22aea622e995e0a
Mira <nurizzatulamiramuzafar@gmail.com>
2016-12-29 20:48:11 +0800

    remove unecessary lines

--- Assets/Script/CreateLevel.cs
+++ Assets/Script/CreateLevel.cs
@@ -8,9 +8,6 @@ public class CreateLevel : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		GameObject obj = Instantiate(Levels[currentLevel-1]) as GameObject;
-		//PlayerPrefs.SetInt ("Level ",1);
-		//currentLevel=PlayerPrefs.GetInt ("Level ");
-		//obj.transform.SetParent(transform,false);
 	}
 	
 	// Update is called once per frame
