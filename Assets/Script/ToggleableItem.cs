f128927e2926cd3767f65932b6f0bd72270e52b6
Mira <nurizzatulamiramuzafar@gmail.com>
2016-12-29 21:14:42 +0800

    recommit the script

---
+++ Assets/Script/ToggleableItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ToggleableItem : MonoBehaviour {
+
+	ToggleableContainer _container;
+    // public AudioSource crash;
+
+    // Allow the parent container to register itself.
+    public void SetContainer(ToggleableContainer container) { 
+        _container = container;
+    }
+
+    // For simple click interactions, Unity will handle the raycasting for you
+    // if you just implement a method to respond to the "OnMouseDown" message.
+    void OnMouseDown() {
+        // Tell our container we've been selected - it will handle coordinating the rest.
+        if(_container != null)
+             _container.Select(this);
+    }
+}
